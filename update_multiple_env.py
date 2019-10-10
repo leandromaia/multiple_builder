c97edbc4dd498b00a883234b42fd586347c78a5c
Leandro Maia <leandroc@inatel.br>
2019-10-10 10:24:43 -0300

    Add add git pwd without real projects path

--- update_multiple_env.py
+++ update_multiple_env.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python
 import os
-import subprocess
+import argparse
+from subprocess import check_call, Popen, PIPE
 
 repo_paths = ('sample_repo_1',
                 'sample_repo_1',
                     'sample_repo_1')
 
-
-def execute_pull(repo_path):
+def execute_pull(repo_path, password=None):
     print(f"********* Starting pull: {repo_path} *****************")
-    subprocess.check_call("git pull", shell=True, cwd=repo_path)
+    proc = Popen(['git', 'pull'], stdin=PIPE)
+    if password:
+        proc.communicate(password)
 
 def execute_gradle_build(repo_path):
     print(f"********* Starting gradle clean build: {repo_path} *****************")
-    subprocess.check_call('gradlew clean build', shell=True, cwd=repo_path)
+    check_call('gradlew clean build', shell=True, cwd=repo_path)
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Options to build projects!')
+    parser.add_argument("--p", default=0, help="This is the Git passphrase!")
+
+    args = parser.parse_args()
+    return args.p
 
 if __name__ == "__main__":
     print(f"********* Starting Repo Update *****************")
+    pwd = get_args()
+
     for repo in repo_paths:
         if os.path.isdir(repo):
-            execute_pull(repo)
+            if pwd != 0:
+                execute_pull(repo, pwd)
+            else:
+                execute_pull(repo)
             execute_gradle_build(repo)
         else:
             print(f"The directory {repo} doesn't exist!!!")
