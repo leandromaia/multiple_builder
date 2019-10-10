0b33b47a1512de4bd7d4fa1c09445f184f475435
Leandro Maia <leandroc@inatel.br>
2019-10-10 17:25:35 -0300

    Add verification to check git pull is alread updated

--- update_multiple_env.py
+++ update_multiple_env.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import os
 import argparse
-from subprocess import check_call, Popen, PIPE
+from subprocess import check_call, Popen, PIPE, check_output
 
-repo_paths = ('sample_repo_1',
-                'sample_repo_1',
-                    'sample_repo_1')
+repo_paths = ('c:/AEL/repositories/com.ericsson.bss.ael.aep.plugins', )
+                # 'sample_repo_1',
+                #     'sample_repo_1')
+
+PULL_UPDATED = "Already up to date"
 
 def execute_pull(repo_path, password=None):
     print(f"********* Starting pull: {repo_path} *****************")
-    proc = Popen(['git', 'pull'], stdin=PIPE)
-    if password:
-        proc.communicate(password)
+    args = ['git', 'pull']
+    # if password:
+    #     args.append('-input')
+    #     args.append(password)
 
+    return str(check_output(args, stdin=PIPE))
+    
 def execute_gradle_build(repo_path):
-    print(f"********* Starting gradle clean build: {repo_path} *****************")
+    print("********* Starting gradle clean build: {repo_path} *****************")
     check_call('gradlew clean build', shell=True, cwd=repo_path)
 
 def get_args():
@@ -25,17 +30,20 @@ def get_args():
     return args.p
 
 if __name__ == "__main__":
-    print(f"********* Starting Repo Update *****************")
+    print("********* Starting Repo Update *****************")
     pwd = get_args()
 
     for repo in repo_paths:
         if os.path.isdir(repo):
+            pull_result = str()
             if pwd != 0:
-                execute_pull(repo, pwd)
+                pull_result = execute_pull(repo, pwd)
             else:
-                execute_pull(repo)
-            execute_gradle_build(repo)
+                pull_result = execute_pull(repo)
+            
+            if PULL_UPDATED not in pull_result:
+                execute_gradle_build(repo)
         else:
             print(f"The directory {repo} doesn't exist!!!")
 
-    print(f"********* Updating finished successfully!!! *****************")
+    print("********* Updating finished successfully!!! *****************")
