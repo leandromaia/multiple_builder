5028f39e5a2839423b644150656aa79b54f4e370
Leandro Maia <leandroc@inatel.br>
2019-10-28 21:12:49 -0300

    Code ajust to run with run method and code refactor

--- update_multiple_env.py
+++ update_multiple_env.py
@@ -1,49 +1,60 @@
 #!/usr/bin/env python
 import os
 import argparse
-from subprocess import check_call, Popen, PIPE, check_output
+import subprocess
 
 
-repo_paths = ('sample_repo_1', )
-                'sample_repo_2',
-                    'sample_repo_3')
+repo_paths = ('c:/AEL/repositories/com.ericsson.bss.ael.aep', 
+                'c:/AEL/repositories/com.ericsson.bss.ael.aep.plugins',
+                    'c:/AEL/repositories/com.ericsson.bss.ael.bae')
+
+# repo_paths = ('com.ericsson.bss.ael.aep', 
+#                 'com.ericsson.bss.ael.aep.plugins',
+#                     'com.ericsson.bss.ael.bae')
 
 PULL_UPDATED = "Already up to date"
 
-def execute_pull(repo_path, password=None):
+def execute_pull(repo_path):
     print(f"********* Starting pull: {repo_path} *****************")
     args = ['git', 'pull']
-    if password:
-        args.append('-input')
-        args.append(password)
+    process = subprocess.run(args, check=True, cwd=repo_path, \
+                            stdout=subprocess.PIPE, universal_newlines=True)
+    return process.stdout
 
-    return str(Popen(args, stdin=PIPE))
+
+def execute_gradle_build(repo_path, build_full):
+    print(f"********* Starting gradle clean build: {repo_path} *************")
     
-def execute_gradle_build(repo_path):
-    print("********* Starting gradle clean build: {repo_path} *****************")
-    check_call('gradlew clean build', shell=True, cwd=repo_path)
+    args = ['gradlew', 'clean', 'build']
+    if not build_full:
+        args = args + ['-x', 'test', '-x', 'check']
+    
+    subprocess.run(args, shell=True, check=True, \
+                                cwd=repo_path, universal_newlines=True)
+    print('Process finished successfully.')
 
-def get_args():
+
+def get_command_args():
     parser = argparse.ArgumentParser(description='Options to build projects!')
-    parser.add_argument("--p", default=0, help="This is the Git passphrase!")
+    parser.add_argument('--build-full', \
+                        '-b', \
+                        action='store_true', \
+                        help="Execute the full gradlew clean build process")
+    return parser.parse_args()
 
-    args = parser.parse_args()
-    return args.p
 
 if __name__ == "__main__":
     print("********* Starting Repo Update *****************")
-    pwd = get_args()
+    args = get_command_args()
 
     for repo in repo_paths:
         if os.path.isdir(repo):
-            pull_result = str()
-            if pwd != 0:
-                pull_result = execute_pull(repo, pwd)
-            else:
-                pull_result = execute_pull(repo)
+            pull_result = execute_pull(repo)
             
             if PULL_UPDATED not in pull_result:
-                execute_gradle_build(repo)
+                execute_gradle_build(repo, args.build_full)
+            else:
+                print(f'The {repo} its already up to date!')
         else:
             print(f"The directory {repo} doesn't exist!!!")
 
