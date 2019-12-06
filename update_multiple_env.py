99b95c7a57ffb32f9b839cb63cb4bfb8d21fd40a
Leandro Maia <leandroc@inatel.br>
2019-12-05 23:06:59 -0300

    Refactor to organize all the menu options to a process class instance

--- update_multiple_env.py
+++ update_multiple_env.py
@@ -7,14 +7,26 @@ from pathlib import Path
 
 
 class Const(object):
-    M2_PATH = ".m2/repository/"
+
+    M2_PATH = ".m2/repository/com/ericsson/bss"
+      
+    REPO_PATHS = ('com.ericsson.bss.ael.aep', 
+                    'com.ericsson.bss.ael.aep.plugins',                    
+                        'com.ericsson.bss.ael.bae',                        
+                            'com.ericsson.bss.ael.dae',                            
+                                'com.ericsson.bss.ael.jive',                                
+                                    'com.ericsson.bss.ael.aep.sdk')
+
     PULL_UPDATED = "Already up to date"
-    REPO_PATHS = ('sample_1',
-                    'sample_2',                    
-                        'sample_3',
-                            'sample_4',
-                                'sample_5',
-                                    'sample_6')
+    
+    # M2_PATH = ".m2/repository/"
+    # REPO_PATHS = ('sample_1',
+    #                 'sample_2',                    
+    #                     'sample_3',
+    #                         'sample_4',
+    #                             'sample_5',
+    #                                 'sample_6')
+
     MAVEN_COMMAND = ['mvn', 'clean','install']
     BUILD_CMDS = {
         1: 'gradlew clean build',
@@ -22,27 +34,28 @@ class Const(object):
         3: 'gradlew clean jar',
         4: 'gradlew clean install'
     }
+    BUILD_BRANCH = 'master'
+    BUILD_BRANCH_OPT = 'M'
 
 
 class HandlerProcess(object):
 
-    def __init__(self, repositories):
-        self._repositories = repositories
+    def __init__(self, process):
+        self._process = process
 
-    def start_process(self, has_deleted_m2, build_full=False, \
-                                    is_to_reset=False, from_menu=False):
-        for repo in self._repositories:
-            pull_result = self._update_repository(\
-                                        repo._absolute_path, is_to_reset)
+    def start_process(self, repositories):
+        for repo in repositories:
+            pull_result = self._update_repository(repo._absolute_path)
             
             if Const.PULL_UPDATED not in pull_result \
-                                or has_deleted_m2 or from_menu:
-                self._build_repository(repo, build_full)
+                            or self._process.clean.is_clean_m2 \
+                                or self._process.is_skip_menu:
+                self._build_repository(repo)
             else:
                 print(f'The {repo.repo_initial} its already up to date!')
 
-    def _update_repository(self, repo_path, is_to_reset):
-        if is_to_reset:
+    def _update_repository(self, repo_path):
+        if self._process.is_to_reset:
             self._update_with_reset(repo_path)
         else:
             print('************ Checkout to branch MASTER ****************')
@@ -66,7 +79,7 @@ class HandlerProcess(object):
         args_reset = ['git', 'reset', '--hard', 'origin/master']
         self._wrapper_run_process(args_reset, repo_path)
 
-    def _build_repository(self, repo, build_full):
+    def _build_repository(self, repo):
         print(f"********* Starting build: {repo._absolute_path} *********")
         self._wrapper_run_process(repo.build_command, repo._absolute_path)
         print("********* Build finished successfully!!! *****************")
@@ -230,6 +243,14 @@ class CliInterface(object):
         user_awser = int(self._get_only_one_awser(menu, indexes))
         return Const.BUILD_CMDS.get(user_awser)
 
+    def ask_wich_build_branch(self):
+        user_awser = input("Which branch all the repositories should to "+ \
+                    "build?\nType only M to default branch master ou type"+ \
+                    " the desired branch name:\nR: ")
+        return Const.BUILD_BRANCH \
+                    if user_awser.upper() == Const.BUILD_BRANCH_OPT \
+                        else user_awser
+
     def _build_menu_options(self, raw_options, indexes=None):
         menu = str()
         list_index = None
@@ -278,39 +299,51 @@ class CliInterface(object):
         return True
 
 
+class Process(object):
+    def __init__(self):
+        self.is_build_full = False
+        self.is_clean_m2 = False
+        self.is_to_reset = False
+        self.is_skip_menu = False
+        self.build_branch = Const.BUILD_BRANCH
+
+
 if __name__ == "__main__":
-    cmd_args_proc = CommandArgsProcessor()    
-    is_build_full = cmd_args_proc.is_build_full()
-    is_clean_m2 = cmd_args_proc.is_to_clean_m2()
-    is_skip_menu = cmd_args_proc.is_to_skip_menu()
+    cmd_args_proc = CommandArgsProcessor()
+
+    process = Process()
+    process.is_build_full = cmd_args_proc.is_build_full()
+    process.is_clean_m2 = cmd_args_proc.is_to_clean_m2()
+    process.is_skip_menu = cmd_args_proc.is_to_skip_menu()
 
     repo_paths = PathHelper.fetch_repo_paths(cmd_args_proc.repos_directory)
 
     list_repo = [Repository(r) for r in repo_paths]
 
     if len(repo_paths) > 0:
-        is_to_reset = False
         gradle_cmd = None
 
-        if not is_skip_menu:
+        if not process.is_skip_menu:
             cli = CliInterface()
             list_repo = cli.ask_desired_repos(list_repo)
-            is_to_reset = cli.ask_is_to_reset()
-            if not is_build_full:
+            process.is_to_reset = cli.ask_is_to_reset()
+            process.build_branch = cli.ask_wich_build_branch()
+            print(process.build_branch)
+
+            if not process.is_build_full:
                 gradle_cmd = cli.ask_type_gradle_build()
-            
+
         if not gradle_cmd:
             gradle_cmd = Const.BUILD_CMDS.get(1)
 
         for r in list_repo:
             r.build_command = gradle_cmd
 
-        if is_clean_m2:
+        if process.is_clean_m2:
             PathHelper.delete_m2(Const.M2_PATH)
 
-        handler = HandlerProcess(list_repo)
-        handler.start_process(is_clean_m2, is_build_full, \
-                                        is_to_reset, is_skip_menu)
+        handler = HandlerProcess(process)
+        handler.start_process(list_repo)
     else:
         print(">>>>>> Failed to read the repositories directories.\n"+
                 ">>>>>> Please make sure you had cloned all the repositories")
