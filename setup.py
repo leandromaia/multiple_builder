fd880bf89ea13f228591d961fd0917ced7c9307b
Leandro Maia <leandroc@inatel.br>
2020-02-27 22:09:36 -0300

    Add personal information in setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='multiple_builder', version='1.0', packages=find_packages())
+setup(
+    name='multiple_builder',
+    version='1.4',
+    packages=find_packages(),
+    author="Leandro de Carvalho Maia",
+    author_email="leandro.maia@ymail.com",
+    description="This is a project used to build multiple environments."
+)
